2aba81a99bbf22583c36d53fa2df75da7927b351
mukashwasti <mkwasti@gmail.com>
2023-10-11 13:49:26 +0500

    fix: chat updated

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -2,18 +2,38 @@ import React from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import {Button } from "react-bootstrap";
-// import SectionOneImg from "../../assets/images/section-one-img.png";
-// import SectionTwoImg from "../../assets/images/section-two-img.png";
-// import SectionThreeImg from "../../assets/images/section-three-img.png";
 import { useNavigate } from "react-router-dom";
 import {
   Robot,
 } from "react-bootstrap-icons";
+interface Chat {
+  type: string;
+  msg: string;
+  agentName: string;
+}
 function Home() {
   const navigate = useNavigate();
   const toSignUp = () => {
     navigate("/signup");
   };
+
+  const [chat] = React.useState<Chat[]>([
+    {type:"agent", msg:"Hello, I'm Lisa from SecureGuard Insurance. How can I assist you today with your insurance needs?", agentName:"Lisa"},
+    {type:"user", msg:"Hi, Lisa. I'd like to inquire about my insurance policy and make a change.", agentName:""},
+    {type:"agent", msg:"Certainly, I can help with that. Could you please provide me with your policy details?", agentName:"Lisa"},
+    {type:"user", msg:"Of course, here's my policy number, AD0898765443 and I'd like to update my coverage.", agentName:""},
+    {type:"agent", msg:"Thank you. Let me check your policy details. It may take a moment.", agentName:""},
+    {type:"user", msg:"No problem, take your time.", agentName:""},
+    {type:"agent", msg:"I've reviewed your policy, and I can certainly help you update your coverage. What changes would you like to make?", agentName:"Lisa"},
+    {type:"user", msg:"I'd like to increase my coverage for home insurance.", agentName:""},
+    {type:"agent", msg:"Not a problem. Let's discuss your options. What specific changes are you looking to make to your home insurance coverage?", agentName:"Lisa"},
+    {type:"user", msg:"I'd like to add coverage for natural disasters, such as floods and earthquakes.", agentName:""},
+    {type:"agent", msg:"Understood. Let me update your policy to include those additional coverages. It may take a moment to process", agentName:""},
+    {type:"user", msg:"I appreciate your assistance with this.", agentName:""},
+    {type:"agent", msg:"our policy has been successfully updated. Is there anything else I can assist you with regarding your insurance needs today?", agentName:"Lisa"},
+    {type:"user", msg:"That's all for now. Thank you for your help, Lisa.", agentName:""},
+    {type:"agent", msg:"You're welcome. If you have any more questions or need further assistance in the future, don't hesitate to reach out. Have a great day!", agentName:"Lisa"},
+  ]);
   return (
     <div className="main-container">
       <section className="main-container-section bg-landing-section-a">
@@ -41,143 +61,39 @@ function Home() {
             <div className="col-xl-8 col-lg-8 col-md-8 col-sm-9 col-9 chat-rows">
               <div className="chat-container">
                 <ul className="chat-box chatContainerScroll">
-                  <li className="chat-left">
-                    <div className="chat-avatar">
-                      <Robot size={50} color="#2f95dc" />
-
-                      <div className="chat-name">Bot</div>
-                    </div>
-                    <div className="chat-text">Hello, I&lsquo;m Bot.
-                      <br />How can I help you today?</div>
-                    <div className="chat-hour"><span className="fa fa-check-circle"></span></div>
-                  </li>
-                  <li className="chat-right">
-                    <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
-                    <div className="chat-text-user">Hi, ETC
-                      <br /> I need more information about Developer Plan.</div>
-                    <div className="chat-avatar">
-                      <div className="chat-name"></div>
-                    </div>
-                  </li>
-                  <li className="chat-left">
-                    <div className="chat-avatar">
-                      <Robot size={50} color="#2f95dc" />
-
-                      <div className="chat-name">Bot</div>
-                    </div>
-                    <div className="chat-text">Are we meeting today?
-                      <br />Project has been already finished and I have results to show you.</div>
-                    <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
-                  </li>
-                  <li className="chat-right">
-                    <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
-                    <div className="chat-text-user">Well I am not sure.
-                      <br />I have results to show you.</div>
-                    <div className="chat-avatar">
-                      <div className="chat-name"></div>
-                    </div>
-                  </li>
-                  <li className="chat-left">
-                    <div className="chat-avatar">
-                      <Robot size={50} color="#2f95dc" />
-
-                      <div className="chat-name">Bot</div>
-                    </div>
-                    <div className="chat-text">The rest of the team is not here yet.
-                      <br />Maybe in an hour or so?</div>
-                    <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
-                  </li>
-                  <li className="chat-right">
-                    <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
-                    <div className="chat-text-user">Have you faced any problems at the last phase of the project?</div>
-                    <div className="chat-avatar">
-                      <div className="chat-name"></div>
-                    </div>
-                  </li>
-                  <li className="chat-left">
-                    <div className="chat-avatar">
-                      <Robot size={50} color="#2f95dc" />
-
-                      <div className="chat-name">Bot</div>
-                    </div>
-                    <div className="chat-text">Actually everything was fine.
-                      <br />Im very excited to show this to our team.</div>
-                    <div className="chat-hour"><span className="fa fa-check-circle"></span></div>
-                  </li>
+                  { chat.map((item, index)=>{
+                    if(item.type==="agent"){
+                      return(
+                        <li className="chat-left" key={index}>
+                          <div className="chat-avatar">
+                            <Robot size={50} color="#2f95dc" />
+                            <div className="chat-name">{item.agentName}</div>
+                          </div>
+                          <div className="chat-text">{item.msg}</div>
+                          <div className="chat-hour"><span className="fa fa-check-circle"></span></div>
+                        </li>
+                      );
+                    }
+                    if(item.type==="user"){
+                      return(
+                        <li className="chat-right" key={index}>
+                          <div className="chat-hour"> <span className="fa fa-check-circle"></span></div>
+                          <div className="chat-text-user">{item.msg}</div>
+                          <div className="chat-avatar">
+                            <div className="chat-name"></div>
+                          </div>
+                        </li>
+                      );
+                    }
+                  
+                  }) 
+                  }
                 </ul>
               </div>
             </div>
-            {/* <Image
-              src={SectionOneImg}
-              alt="Omni Service Logo"
-              className="section-img"
-            /> */}
           </div>
         </div>
       </section>
-      {/* <section className="main-container-section">
-        <div className="d-flex flex-wrap align-items-center justify-content-between content-inner-section">
-          <div className="inner-content">
-            <Image
-              src={SectionTwoImg}
-              alt="Omni Service Logo"
-              className="section-img"
-            />
-          </div>
-          <div className="inner-content text-right">
-            <p className="section-heading text-42">
-              Reinvent customer service and automate sales
-            </p>
-            <p className="section-text">
-              Welcome to Omniservice, the future of customer support. Our
-              AI-powered virtual agents speak multiple languages and can assist
-              your customers on any channel, 24/7.
-            </p>
-            <p className="section-text">
-              Are you tired of the limitations of traditional customer support?
-              Omniservice is here to revolutionize the industry with our
-              omnichannel conversational virtual agents.
-            </p>
-            <p className="section-text">
-              Omniservice virtual agents can handle a wide range of customer
-              inquiries, from simple FAQs to complex transactions. Plus, our
-              advanced NLP technology ensures accurate and natural language
-              processing.
-            </p>
-            <Button
-              className="submit-btn p-3 sora-regular btn btn-primary"
-              onClick={toSignUp}
-            >
-              Learn More
-            </Button>
-          </div>
-        </div>
-      </section> */}
-      {/* <section className="main-container-section  bg-landing-section-a">
-        <div className="d-flex flex-wrap align-items-center justify-content-between content-inner-section">
-          <div className="inner-content">
-            <p className="section-heading text-42">Voice and Chat</p>
-            <p className="section-text">
-              With Omniservice&apos;s voice and chat capabilities, you can provide
-              exceptional customer experiences and build lasting relationships
-              with your customers.
-            </p>
-            <Button
-              className="submit-btn p-3 sora-regular btn btn-primary"
-              onClick={toSignUp}
-            >
-              Learn More
-            </Button>
-          </div>
-          <div className="inner-content  text-right">
-            <Image
-              src={SectionThreeImg}
-              alt="Omni Service Logo"
-              className="section-img"
-            />
-          </div>
-        </div>
-      </section> */}
       <Footer />
     </div>
   );
