2c427fcc9deb831853b629cb02c8db0a2f58c972
Placidus Agukwe <cloudcorp.emea@gmail.com>
2025-05-28 17:00:56 +0000

    Set up GitHub Action for Firebase Hosting deploy

--- src/app/pages/signUp/index.tsx
+++ src/app/pages/signUp/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { errorAlert, validateEmail } from "../../utils/HelperMixins";
 import { SignUpProps } from "./utils";
